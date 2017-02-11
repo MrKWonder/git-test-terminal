c863aafbca88d5c60ec22be966a6947b99670ad2
Keelan Shaw <KeelanShaw@keelanshawsimac.home>
2017-02-11 13:58:12 +0000

    The function is nearly ready for arrays of any size, but I've left an error on purpose.

--- PractiseMergingArray.playground/Contents.swift
+++ PractiseMergingArray.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 
 var arr1 = [1, 4, 7, 10, 13]
 var arr2 = [2, 5, 8, 11, 14]
-var arr3 = [3, 6, 9, 12, 15]
+var arr3 = [3, 6, 9, 12, 15, 18, 21]
 
 func countingNumbers(set1: [Int], set2: [Int], set3: [Int]) -> [Int] {
 
@@ -14,20 +14,20 @@ func countingNumbers(set1: [Int], set2: [Int], set3: [Int]) -> [Int] {
 //        maxCount = set1.count
 //    }
     
-    let maxCount = set1.count
+    let maxCount = max(set1.count, set2.count, set3.count)
     var combinedArray = [Int]()
     
     for x in 0..<maxCount {
         
-        if x <= set1.count {
+        if set1.count  x {
             combinedArray.append(set1[x])
         }
         
-        if x <= set2.count {
+        if set2.count > x {
             combinedArray.append(set2[x])
         }
         
-        if x <= set3.count {
+        if set3.count > x {
             combinedArray.append(set3[x])
         }
         
